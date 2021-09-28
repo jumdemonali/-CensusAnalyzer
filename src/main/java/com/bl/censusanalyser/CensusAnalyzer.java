3d62b4a39250cc94417f0c1524cf08d67dca66f8
jumdemonali <jumdemonali@gmail.com>
2021-09-28 12:39:12 +0530

    Added: Welcome message

---
+++ src/main/java/com/bl/censusanalyser/CensusAnalyzer.java
@@ -0,0 +1,7 @@
+package com.bl.censusanalyser;
+
+public class CensusAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Welcome to census analyzer program!!");
+    }
+}
